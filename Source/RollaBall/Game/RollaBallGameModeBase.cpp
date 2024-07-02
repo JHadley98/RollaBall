9ed6338d027a9e8ad1d3956f1392a59cd89eb131
JHadley98 <james.hadley1@live.co.uk>
2024-07-02 14:35:47 +0100

    Created Game Mode Base
    Create custom game mode to set the player, HUD and logic for collecting items in the game.

---
+++ Source/RollaBall/Game/RollaBallGameModeBase.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RollaBallGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+#include "RollaBall/Items/RollaBallItemBase.h"
+
+void ARollaBallGameModeBase::BeginPlay()
+{
+	TArray<AActor*> Items;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ARollaBallItemBase::StaticClass(), Items);
+	ItemsInLevel = Items.Num();
+
+
+}
+
+void ARollaBallGameModeBase::UpdateItemText()
+{
+
+}
+
+void ARollaBallGameModeBase::ItemCollected()
+{
+	ItemsCollected++;
+}
